3c792d8fa9ae65714b9d1babd61d6aac931f0742
kavin-x <kavinkumarab@gamil.com>
2022-08-29 19:44:40 +0530

    migrated models

--- backend/store/models.py
+++ backend/store/models.py
@@ -1,10 +1,5 @@
-from distutils.command.upload import upload
-from importlib.util import set_loader
-from operator import mod
-from tabnanny import verbose
-from turtle import ondrag
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Category(models.Model):
     name= models.CharField(max_length=255,db_index=True)
@@ -19,10 +14,10 @@ class Category(models.Model):
 
 class Product(models.Model):
     category= models.ForeignKey(Category,related_name='product',on_delete=models.CASCADE)
-    Category= models.ForeignKey(User,related_name='product_creator',on_delete=models.CASCADE)
+    created_by= models.ForeignKey(User,related_name='product_creator',on_delete=models.CASCADE)
     author= models.CharField(max_length=255,default='admin')
     title= models.CharField(max_length=255)
-    description= models.CharField(blank=True)
+    description= models.CharField(max_length=255,blank=True)
     image= models.ImageField(upload_to='images/')
     slug = models.SlugField(max_length=255,db_index=True)
     price = models.DecimalField(max_digits=4,decimal_places=2)
