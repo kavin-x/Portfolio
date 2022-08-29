d9ef801ce05fd99edd37a9f7f301e02b9b76e7e7
kavin-x <kavinkumarab@gamil.com>
2022-08-29 19:09:25 +0530

    inital django setup

---
+++ backend/store/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class category(models.Model):
+    name= models.CharField(max_length=255,db_index=True)
+    slug = models.SlugField(max_length=255,db_index=True)
