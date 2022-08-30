bff8675109e1ca36f1ea6f21978cd1e894a26787
kavin-x <kavinkumarab@gamil.com>
2022-08-30 12:02:19 +0530

    cretaed url patterns

--- backend/store/views.py
+++ backend/store/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Category,Product
 # Create your views here.
+def allProducts(request):
+    product=Product.objects.all()
+    print(product)
+    return render(request,'store/home.html',{'product':product})
